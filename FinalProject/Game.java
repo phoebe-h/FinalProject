55117af2794483c9768d42de778e9bb3fde0825f
JaimitaZhang <jxzhang@stu.naperville203.org>
2017-05-19 10:29:31 -0500

    tweakin
    added gravity
    random generated gangsters and tried to do same with minhyuk but failed

--- FinalProject/Game.java
+++ FinalProject/Game.java
@@ -15,16 +15,13 @@ public class Game extends Applet implements Runnable, KeyListener
     private final int APPLET_HEIGHT = 600;
     private final int HEIGHT_MIN = 100;
     private final int VARIANCE = 40;
-    //    private int y;
     private int dy;
-    //    private int dx;
-   
     private Ground bar1;
     private User user;
-       private BongSoon bong1;
-    //    private MinHyuk min1;
-    //    private SooTak soo1;
-        private GookDu gook1;
+    private BongSoon bong1;
+    private MinHyuk min1;
+    private SooTak soo1, soo2, soo3;
+    private GookDu gook1;
     
     private Stars stars; 
     private int count; 
@@ -38,6 +35,8 @@ public class Game extends Applet implements Runnable, KeyListener
     private Skyline skyline; 
     private int bongx, bongy; 
     private char ch; 
+    private int randomsoo1, randomsoo2, randomsoo3, soox1, soox2, soox3; 
+    private int randommin, minx; 
     
     public void keyPressed (KeyEvent e) {}
     public void keyReleased (KeyEvent e) {}
@@ -47,7 +46,7 @@ public class Game extends Applet implements Runnable, KeyListener
             {bongx+=20; 
              bong1.move(bongx,bongy); }
         if (ch=='w')
-            {bongy-=20; 
+            {bongy-=30; 
             bong1.move(bongx,bongy);}
         if (ch=='s')
             {bongy+=20; 
@@ -58,15 +57,18 @@ public class Game extends Applet implements Runnable, KeyListener
     
     public void init()
     {       addKeyListener(this); 
-            //initializes ground objects
             bar1 = new Ground(0, APPLET_HEIGHT-67);         
             
             bongx = 0; 
-            bongy = APPLET_HEIGHT-300; 
+            bongy = APPLET_HEIGHT-115; 
             bong1 = new BongSoon(bongx, bongy);
-        //    min1 = new MinHyuk();
-        //    soo1 = new SooTak();
-            gook1 = new GookDu();
+
+            soox1=700; 
+            soox2=700;
+            soox3=700;
+            
+            minx=700; 
+  
             stars = new Stars(50,50);
             count = 0; 
             r=3; 
@@ -80,7 +82,7 @@ public class Game extends Applet implements Runnable, KeyListener
             background = new Background(r,g,b);
             circle = new RandomCircle(400,y1,r,g,b);
             skyline = new Skyline (0,APPLET_HEIGHT-450); 
-            
+
             //sets size of applet
             setSize(APPLET_WIDTH, APPLET_HEIGHT);
     }
@@ -90,8 +92,7 @@ public class Game extends Applet implements Runnable, KeyListener
         while (done){
             //increments count to keep track of runs of the while loop
             count++;
-            
-            
+
             //moon moves down as long as the y-coordinate is less than 600
             if (y1<=600) 
                 if (count%5==0)
@@ -127,8 +128,57 @@ public class Game extends Applet implements Runnable, KeyListener
             background.setColors(r,g,b);
             circle = new RandomCircle(400,y1,r,g,b);}
             
+            //simulates gravity 
+            if (bongy<(APPLET_HEIGHT-115))
+                {bongy+=10; 
+                bong1.move(bongx,bongy); }
+            
+            randomsoo1 = (int)(Math.random()*10+1); 
+            //generates gangsters at random times
+            if (count%randomsoo1==0)
+                {soo1 = new SooTak();    
+                if (soox1>=0)
+                    {soox1-=20; 
+                     soo1.move(soox1,APPLET_HEIGHT-115); }}
+            if (soox1<=0)
+                soox1=700; 
+                
+            randomsoo2 = (int)(Math.random()*10+1); 
+            //generates gangsters at random times
+            if (count%randomsoo2==0)
+                {soo2 = new SooTak();    
+                if (soox2>=0)
+                    {soox2-=20; 
+                     soo2.move(soox2,APPLET_HEIGHT-115); }}
+            if (soox2<=0)
+                soox2=700; 
+                
+                
+            randomsoo3 = (int)(Math.random()*10+1); 
+            //generates gangsters at random times
+            if (count%randomsoo3==0)
+                {soo3 = new SooTak();    
+                if (soox3>=0)
+                    {soox3-=30; 
+                     soo3.move(soox3,APPLET_HEIGHT-115); }}
+            if (soox3<=0)
+                soox3=700; 
+                     
+                
+            randommin = (int)(Math.random()*10+1); 
+            //generates minhyuk at random times
+            if (count%randommin==0)
+                {min1 = new MinHyuk();    
+                if (minx>=0)
+                    {minx-=20;  
+                     min1.move(minx,APPLET_HEIGHT-115); }}
+            if (minx<=0)
+                minx=700; 
+                     
+                     
             if (count==10000)
-            {done=false;}
+                done=false;
+            
         
         }
    }
@@ -166,8 +216,10 @@ public class Game extends Applet implements Runnable, KeyListener
             }
       gook1.move(page, 50, 50);*/
       bong1.draw(page);
-      
-      
+      soo1.draw(page); 
+      soo2.draw(page); 
+      soo3.draw(page); 
+      min1.draw(page); 
 
    }
     
